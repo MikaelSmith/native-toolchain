aa1276de04f4a32b291c6aaaaf6ab7d7423b33b7
Michael Smith <michael.smith@cloudera.com>
2026-09-08 15:24:24 -0700

    Try newer binutils 2.46.1 with gold
    Linking with older gold on RHEL 8 is producing a slightly mangled binary
    and breakpad is having issues with alignment. Try newer gold.
    
    Change-Id: I471e5d961cf6e7842514177515ea05115b652e85

--- init.sh
+++ init.sh
@@ -75,7 +75,7 @@ export CLEAN
 : ${CLEAN_TMP_AFTER_BUILD=0}
 export CLEAN_TMP_AFTER_BUILD
 
-: ${BINUTILS_VERSION=2.44}
+: ${BINUTILS_VERSION=with-gold-2.46.1}
 export BINUTILS_VERSION
 
 # Flag to determine the system compiler is used
